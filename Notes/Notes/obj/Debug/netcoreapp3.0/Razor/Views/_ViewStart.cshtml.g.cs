4fa34cd41491088ff08ba1aa716807719ef7efcb
Denis Makarenko <denis-makarenko@outlook.com>
2020-01-15 07:17:37 +0300

    Add some class for NoteComponent

--- Notes/Notes/obj/Debug/netcoreapp3.0/Razor/Views/_ViewStart.cshtml.g.cs
+++ Notes/Notes/obj/Debug/netcoreapp3.0/Razor/Views/_ViewStart.cshtml.g.cs
@@ -26,8 +26,15 @@ using Notes.Views.Components;
 #line default
 #line hidden
 #nullable disable
+#nullable restore
+#line 3 "C:\repos\Examples\Notes\Notes\Views\_ViewImports.cshtml"
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+#line default
+#line hidden
+#nullable disable
     [global::Microsoft.AspNetCore.Razor.Hosting.RazorSourceChecksumAttribute(@"SHA1", @"7091c65830b0329e613be026ede8a57552863778", @"/Views/_ViewStart.cshtml")]
-    [global::Microsoft.AspNetCore.Razor.Hosting.RazorSourceChecksumAttribute(@"SHA1", @"236b5707b81c6a2e32b6fff0130f367c2587a29a", @"/Views/_ViewImports.cshtml")]
+    [global::Microsoft.AspNetCore.Razor.Hosting.RazorSourceChecksumAttribute(@"SHA1", @"ced66711d8fd3974906c3d09c051bd93d5426e2a", @"/Views/_ViewImports.cshtml")]
     public class Views__ViewStart : global::Microsoft.AspNetCore.Mvc.Razor.RazorPage<dynamic>
     {
         #pragma warning disable 1998
