3ff83e6c95fd4114de9c185d2341a3cb523706d2
Denis Makarenko <denis-makarenko@outlook.com>
2020-01-16 21:59:09 +0300

    Changed project to .net core 3.1

--- Notes/Notes/Controllers/NoteController.cs
+++ Notes/Notes/Controllers/NoteController.cs
@@ -1,33 +1,41 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Notes.Models;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Notes.Controllers
 {
     public class NoteController : Controller
     {
+        private List<Note> _notes;
         public IActionResult Index()
         {
-            var notes = new List<Note>
+            _notes = new List<Note>
             {
                 new Note
                 {
+                    Id = 1,
                     Title="Hello",
                     Description= "Description"
                 },
                 new Note
                 {
+                    Id =2,
                     Title="Hello",
                     Description= "Description"
                 },
                 new Note
                 {
+                    Id =3,
                     Title="Hello",
                     Description= "Description"
                 }
             };
 
-            return View(notes);
+            return View(_notes);
         }
+
+        public IActionResult Edit(int noteId)
+            => View();
     }
 }
