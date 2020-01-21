bd3c604af8c2a84408218186af48bfb2e7dd9035
Denis Makarenko <denis-makarenko@outlook.com>
2020-01-21 07:18:21 +0300

    Add new component: EditNote.razor

--- Notes/Notes/Models/Note.cs
+++ Notes/Notes/Models/Note.cs
@@ -1,11 +1,15 @@
-﻿namespace Notes.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Notes.Models
 {
     public class Note
     {
         public int Id { get; set; }
 
+        [Required]
         public string Title { get; set; }
 
+        [Required]
         public string Description { get; set; }
     }
 }
