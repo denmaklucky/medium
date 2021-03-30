d3e6a8a9087387ed4dfa625723da4d49d149da86
Denis Makarenko <denis-makarenko@outlook.com>
2021-03-30 21:08:58 +0300

    Removed mongodatabase

--- BlazorWebassembly/Metatable/Metatable/Services/MetaService.cs
+++ BlazorWebassembly/Metatable/Metatable/Services/MetaService.cs
@@ -1,4 +1,6 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 using Metatable.Models;
 
 namespace Metatable.Services
@@ -8,24 +10,28 @@ namespace Metatable.Services
         void CreateTable(TableModel table);
         Task UpdateTable(TableModel table);
         Task<TableModel> GetTable();
+        int GetTableCount();
     }
 
     public class MetaService : IMetaService
     {
-        private readonly IAppRepository _repository;
+        private static List<TableModel> _repository;
 
-        public MetaService(IAppRepository repository)
-            => _repository = repository;
+        public MetaService()
+            => _repository = new List<TableModel>();
 
         public void CreateTable(TableModel table)
         {
-            _repository.CreateTable(table);
+            _repository.Add(table);
         }
 
         public Task UpdateTable(TableModel table)
-            => _repository.UpdateTable(table.Id, table);
+            => throw new NotImplementedException(); //_repository.UpdateTable(table.Id, table);
 
         public Task<TableModel> GetTable()
-            => _repository.GetFirstTable();
+            => throw new NotImplementedException(); // _repository.GetFirstTable();
+
+        public int GetTableCount()
+            => _repository.Count;
     }
 }
