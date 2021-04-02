41a4e10367b75a2f88d1f6130302b6fca7219689
Denis Makarenko <denis-makarenko@outlook.com>
2021-04-02 16:34:41 +0300

    Add modal

--- BlazorWebassembly/Metatable/Metatable/Program.cs
+++ BlazorWebassembly/Metatable/Metatable/Program.cs
@@ -4,6 +4,7 @@ using System;
 using System.Net.Http;
 using System.Threading.Tasks;
 using Metatable.Services;
+using Blazored.Modal;
 
 namespace Metatable
 {
@@ -15,6 +16,7 @@ namespace Metatable
             builder.RootComponents.Add<App>("app");
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services.AddBlazoredModal();
             builder.Services.AddScoped<IMetaService, MetaService>();
 
             await builder.Build().RunAsync();
