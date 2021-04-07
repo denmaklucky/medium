7a9e0f09ecfbff3faa168b315621b63569def582
Denis Makarenko <denis-makarenko@outlook.com>
2021-04-07 20:59:02 +0300

    Add modal dialog for editing property

--- BlazorWebassembly/Metatable/Metatable/Models/MetaProperty.cs
+++ BlazorWebassembly/Metatable/Metatable/Models/MetaProperty.cs
@@ -4,14 +4,6 @@ namespace Metatable.Models
 {
     public class MetaProperty
     {
-        public MetaProperty(string name, object value, PropertyType type, bool isHidden = false)
-        {
-            Name = name;
-            Value = value;
-            Type = type;
-            IsHidden = isHidden;
-        }
-
         public string Name { get; set; }
         public object Value { get; set; }
         public PropertyType Type { get; set; }
