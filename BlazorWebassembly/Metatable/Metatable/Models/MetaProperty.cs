85399388d0c7313003ab7910b05654f371c85ade
Denis Makarenko <denis-makarenko@outlook.com>
2021-04-28 17:39:18 +0300

    Added possobility for chanching and addding a new property in object

--- BlazorWebassembly/Metatable/Metatable/Models/MetaProperty.cs
+++ BlazorWebassembly/Metatable/Metatable/Models/MetaProperty.cs
@@ -5,7 +5,7 @@ namespace Metatable.Models
     public class MetaProperty
     {
         public string Name { get; set; }
-        public object Value { get; set; }
+        public string Value { get; set; }
         public PropertyType Type { get; set; }
         public bool IsHidden { get; set; }
     }
