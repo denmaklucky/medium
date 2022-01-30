ef8980d20ddad53beec17156c59615ff6a7591ba
Denis Makarenko <denis-makarenko@outlook.com>
2022-01-30 19:23:06 +0200

    Some update

--- WebApp/RazorPlusVueJs/RazorPlusVueJs/Pages/RazorVue.cshtml.cs
+++ WebApp/RazorPlusVueJs/RazorPlusVueJs/Pages/RazorVue.cshtml.cs
@@ -1,11 +1,31 @@
-﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using RazorPlusVueJs.Entities;
+using RazorPlusVueJs.Models;
 
 namespace RazorPlusVueJs.Pages;
 
 public class RazorVue : PageModel
 {
+    public RazorVue(IToDoList toDoList)
+    {
+        List = toDoList;
+    }
+
     public void OnGet()
     {
-        
     }
+
+    public IActionResult OnPost(string newTask)
+    {
+        if (string.IsNullOrEmpty(newTask))
+            return RedirectToPage("RazorVue");
+
+        List.Items.Add(new ToDoItem(newTask));
+        List.Save();
+
+        return RedirectToPage("RazorVue");
+    }
+
+    public IToDoList List { get; }
 }
