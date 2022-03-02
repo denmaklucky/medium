a4e48d641e689ef0ed1342fc2c769ffe801ad548
Denis Makarenko <denis-makarenko@outlook.com>
2022-03-02 21:46:04 +0200

    Add logic to `sign-up`

--- WebApp/ToDoList/WebApp/Pages/Signup.cshtml.cs
+++ WebApp/ToDoList/WebApp/Pages/Signup.cshtml.cs
@@ -1,11 +1,22 @@
 ﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Options;
+using WebApp.Options;
 
 namespace WebApp.Pages;
 
 public class Signup : PageModel
 {
+    private readonly AppOptions _options;
+
+    public Signup(IOptions<AppOptions> options)
+    {
+        _options = options.Value;
+    }
+    
     public void OnGet()
     {
         
     }
+
+    public string BaseUrl => _options.BaseUrl;
 }
