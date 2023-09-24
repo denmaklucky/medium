7901b68926818ff4f1ef7b26cdc4e443af266388
Denis Makarenko <denis-makarenko@outlook.com>
2023-09-24 17:23:41 +0300

    Added a new project `Analyzer`

---
+++ Roslyn/Analyzer/SealedClassAnalyzer/SealedClassCodeFix.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Immutable;
+using System.Composition;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CodeActions;
+using Microsoft.CodeAnalysis.CodeFixes;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace SealedClassAnalyzer;
+
+[Shared]
+[ExportCodeFixProvider(LanguageNames.CSharp, Name = nameof(SealedClassCodeFix))]
+public sealed class SealedClassCodeFix : CodeFixProvider
+{
+    public override ImmutableArray<string> FixableDiagnosticIds => ImmutableArray.Create(SealedClassRule.RuleId);
+
+    public override FixAllProvider GetFixAllProvider() => WellKnownFixAllProviders.BatchFixer;
+
+    public override async Task RegisterCodeFixesAsync(CodeFixContext context)
+    {
+        var diagnostic = context.Diagnostics.First();
+        var diagnosticSpan = diagnostic.Location.SourceSpan;
+        
+        var root = await context.Document.GetSyntaxRootAsync(context.CancellationToken);
+
+        var classDeclaration = root?.FindToken(diagnosticSpan.Start).Parent?.AncestorsAndSelf().OfType<ClassDeclarationSyntax>().First();
+
+        if (classDeclaration == null)
+        {
+            return;
+        }
+
+        context.RegisterCodeFix(
+            CodeAction.Create(
+                title: "Make the class sealed",
+                createChangedDocument: _ => MakeClassSealedAsync(root!, context.Document, classDeclaration),
+                equivalenceKey: nameof(SealedClassCodeFix)),
+            diagnostic);
+    }
+
+    private static Task<Document> MakeClassSealedAsync(SyntaxNode root, Document document, ClassDeclarationSyntax @class)
+    {
+        var newClass = @class.WithModifiers(@class.Modifiers.Add(SyntaxFactory.Token(SyntaxKind.SealedKeyword)));
+
+        var newRoot = root.ReplaceNode(@class, newClass);
+
+        return Task.FromResult(document.WithSyntaxRoot(newRoot));
+    }
+}
---
+++ Roslyn/Analyzer/SealedClassAnalyzer/SealedClassRule.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Immutable;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+
+namespace SealedClassAnalyzer;
+
+[DiagnosticAnalyzer(LanguageNames.CSharp)]
+public sealed class SealedClassRule : DiagnosticAnalyzer
+{
+    internal const string RuleId = "RULE0001";
+    
+    private static readonly DiagnosticDescriptor Rule = new(
+        RuleId,
+        title: "The class have to be sealed",
+        messageFormat: "Class can be sealed",
+        category: "Unknown",
+        DiagnosticSeverity.Warning,
+        isEnabledByDefault: true);
+
+    public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create(Rule);
+
+    public override void Initialize(AnalysisContext context)
+    {
+        context.EnableConcurrentExecution();
+        context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.None);
+        context.RegisterSyntaxNodeAction(AnalyzeSealedClass, SyntaxKind.ClassDeclaration);
+    }
+
+    private static void AnalyzeSealedClass(SyntaxNodeAnalysisContext context)
+    {
+        var classSyntax = (ClassDeclarationSyntax)context.Node;
+
+        var skipRuleGeneration = classSyntax.Modifiers.Any(modifier => modifier.IsKind(SyntaxKind.AbstractKeyword) ||
+                                                                       modifier.IsKind(SyntaxKind.SealedKeyword));
+
+        if (skipRuleGeneration)
+        {
+            return;
+        }
+
+        var diagnostic = Diagnostic.Create(Rule, classSyntax.Keyword.GetLocation());
+        context.ReportDiagnostic(diagnostic);
+    }
+}
