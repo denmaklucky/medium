e7b811923cb450026c4d3a14fd9d5a2531f3958d
Denis Makarenko <denis-makarenko@outlook.com>
2024-08-19 09:56:09 +0200

    added a new project about BestIndetifier

---
+++ CSharp/BestIndetifier/BestIdentifier/IdentifierBenchmark.cs
@@ -0,0 +1,18 @@
+﻿using BenchmarkDotNet.Attributes;
+
+namespace BestIdentifier;
+
+public class IdentifierBenchmark
+{
+    [Benchmark]
+    public void GuidV4() => Guid.NewGuid();
+
+    [Benchmark]
+    public void GuidV7() => Guid.CreateVersion7();
+
+    [Benchmark]
+    public void Ulid() => System.Ulid.NewUlid();
+
+    [Benchmark]
+    public void Suid() => BestIdentifier.Suid.New();
+}
---
+++ CSharp/BestIndetifier/BestIdentifier/Program.cs
@@ -0,0 +1,4 @@
+﻿using BenchmarkDotNet.Running;
+using BestIdentifier;
+
+BenchmarkRunner.Run<IdentifierBenchmark>();
