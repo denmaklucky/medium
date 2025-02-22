d7120159378264b2ce164554e8f351d1ad852ce8
Denis Makarenko <denis-makarenko@outlook.com>
2025-02-22 16:07:24 +0100

    Added repository and endpoints which show how we can implement idempotentoncy

---
+++ WebAPI/Idempotency/IdempotentApp/IdempotentApp/NoteRepository.cs
@@ -0,0 +1,34 @@
+﻿using System.Data;
+using Dapper;
+
+namespace IdempotentApp;
+
+internal sealed class NoteRepository(IDbConnection dbConnection)
+{
+    public async Task RegisterAsync(Guid id, string? value)
+    {
+        await dbConnection.ExecuteAsync(
+            "INSERT INTO Notes (Id, Value, CreatedAt) " +
+            "VALUES (@Id, @Value, @Now) " +
+            "ON DUPLICATE KEY UPDATE " +
+            "Value = @Value, " +
+            "UpdatedAt = @Now;",
+            new
+            {
+                Id = id,
+                Value = value,
+                Now = DateTimeOffset.UtcNow
+            });
+    }
+
+    public async Task<IReadOnlyList<NoteModel>> ListAsync()
+    {
+        var result = await dbConnection.QueryAsync<NoteModel>(
+            "SELECT Id, Value, CreatedAt, UpdatedAt " +
+            "FROM Notes;");
+
+        return result.ToList();
+    }
+
+    public sealed record NoteModel(Guid Id, string? Value, DateTime CreatedAt, DateTime? UpdatedAt);
+}
--- WebAPI/Idempotency/IdempotentApp/IdempotentApp/Program.cs
+++ WebAPI/Idempotency/IdempotentApp/IdempotentApp/Program.cs
@@ -1,6 +1,42 @@
+using System.Data;
+using IdempotentApp;
+using Microsoft.AspNetCore.Mvc;
+using MySql.Data.MySqlClient;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddScoped<IDbConnection>(_ =>
+{
+    const string connectionStringName = "OwlDb";
+
+    var connectionString = builder.Configuration.GetConnectionString(connectionStringName);
+
+    return new MySqlConnection(connectionString);
+});
+
+builder.Services.AddScoped<NoteRepository>();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapPut("/notes/{noteId:guid}", async ([FromRoute] Guid? noteId, [FromBody] RegisterNoteRequest request, [FromServices] NoteRepository repository) =>
+{
+    if (noteId == null || noteId == Guid.Empty)
+    {
+        return Results.Problem("Note Id cannot be null or empty.", statusCode: 400);
+    }
+
+    await repository.RegisterAsync(noteId.Value, request.Value);
+
+    return Results.Ok();
+});
+
+app.MapGet("/notes", async ([FromServices] NoteRepository repository) =>
+{
+    var list = await repository.ListAsync();
+
+    return Results.Ok(list);
+});
 
 app.Run();
+
+record RegisterNoteRequest(string? Value);
