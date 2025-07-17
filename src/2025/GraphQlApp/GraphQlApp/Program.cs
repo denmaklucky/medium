1a14dc1ca5f99c9cb2bc19d00b547a828eeb36d6
Denis Makarenko <denis-makarenko@outlook.com>
2025-07-17 19:15:07 +0200

    last changes

--- src/2025/GraphQlApp/GraphQlApp/Program.cs
+++ src/2025/GraphQlApp/GraphQlApp/Program.cs
@@ -5,12 +5,63 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDbContext<WeatherDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("OwlDb")));
 
+builder.Services
+    .AddGraphQLServer()
+    .AddQueryType<WeatherQuery>()
+    .AddProjections()
+    .AddFiltering()
+    .AddSorting();
+
 var app = builder.Build();
 
 using var scope = app.Services.CreateScope();
-var dbContext = scope.ServiceProvider.GetRequiredService<WeatherDbContext>();
-await DbSeeder.SeedAsync(dbContext);
+var context = scope.ServiceProvider.GetRequiredService<WeatherDbContext>();
+await DbSeeder.SeedAsync(context);
 
-app.MapGet("/", () => "Hello World!");
+// app.MapGet("/countries", async ([FromServices] WeatherDbContext dbContext) =>
+// {
+//     var countries = await dbContext.Countries
+//         .Select(country => new { country.CountryCode })
+//         .ToListAsync();
+//     
+//     return TypedResults.Ok(countries);
+// });
+//
+// app.MapGet("/countries/{countryCode}/cities", async (
+//     [FromRoute] string countryCode,
+//     [FromServices] WeatherDbContext dbContext) =>
+// {
+//     var cities = await dbContext.Cities
+//         .Include(city => city.Country)
+//         .Where(city => city.Country!.CountryCode == countryCode)
+//         .Select(city => new { city.Id, city.Name })
+//         .ToListAsync();
+//     
+//     return TypedResults.Ok(cities);
+// });
+//
+// app.MapGet("/cities/{cityId:int}/forecast", async (
+//     [FromRoute] int cityId,
+//     [FromServices] WeatherDbContext dbContext) =>
+// {
+//     var forecast = await dbContext.WeatherForecasts
+//         .Include(forecast => forecast.City)
+//         .Where(forecast => forecast.City!.Id == cityId)
+//         .Select(forecast => new
+//         {
+//             forecast.TemperatureHigh,
+//             forecast.TemperatureLow,
+//             forecast.Humidity,
+//             forecast.Pressure,
+//             forecast.WindSpeed,
+//             forecast.WindDirection,
+//             forecast.Description
+//         })
+//         .ToListAsync();
+//     
+//     return TypedResults.Ok(forecast);
+// });
+
+app.MapGraphQL();
 
 app.Run();
---
+++ src/2025/GraphQlApp/GraphQlApp/WeaterQuery.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace GraphQlApp;
+
+public class WeatherQuery
+{
+    [UseProjection]
+    [UseFiltering]
+    [UseSorting]
+    public IQueryable<Country> GetCountries([Service] WeatherDbContext context)
+        => context.Countries;
+    
+    [UseProjection]
+    [UseFiltering]
+    [UseSorting]
+    public IQueryable<City> GetCities([Service] WeatherDbContext context)
+        => context.Cities;
+    
+    [UseProjection]
+    [UseFiltering]
+    [UseSorting]
+    public IQueryable<WeatherForecast> GetWeatherForecasts([Service] WeatherDbContext context)
+        => context.WeatherForecasts;
+    
+    public Task<Country?> GetCountryByCode(string countryCode, [Service] WeatherDbContext context)
+        => context.Countries.FirstOrDefaultAsync(country => country.CountryCode == countryCode);
+    
+    public Task<List<City>> GetCitiesByCountry(int countryId, [Service] WeatherDbContext context)
+        => context.Cities.Where(city => city.CountryId == countryId).ToListAsync();
+    
+    public Task<List<WeatherForecast>> GetForecastsForCity(int cityId, int days, [Service] WeatherDbContext context)
+        => context.WeatherForecasts
+            .Where(weatherForecast => weatherForecast.CityId == cityId &&
+                                      weatherForecast.ForecastDate >= DateTime.Today &&
+                                      weatherForecast.ForecastDate <= DateTime.Today.AddDays(days))
+            .OrderBy(wf => wf.ForecastDate)
+            .ToListAsync();
+}
