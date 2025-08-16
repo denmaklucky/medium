2e5a1b225a1b3ed516c796f37cecd362b972746d
Denis Makarenko <denis-makarenko@outlook.com>
2025-08-16 12:45:47 +0200

    test

---
+++ src/2025/OrmApp/OrmApp/AppDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace OrmApp;
+
+public sealed class AppDbContext(string connectionString) : DbContext
+{
+    public DbSet<User> Users { get; set; } = null!;
+    
+     protected override void OnConfiguring(DbContextOptionsBuilder options)
+        => options.UseSqlServer(connectionString);
+    
+    protected override void OnModelCreating(ModelBuilder bilder)
+    {
+        bilder.Entity<User>(user =>
+        {
+            user.ToTable("Users");
+            user.HasKey(x => x.Id);
+            user.Property(x => x.Id).ValueGeneratedNever();
+            user.Property(x => x.Email).HasMaxLength(200).IsRequired();
+            user.HasIndex(x => x.Email).IsUnique(false);
+        });
+    }
+}
---
+++ src/2025/OrmApp/OrmApp/OrmBench.cs
@@ -0,0 +1,78 @@
+﻿using BenchmarkDotNet.Attributes;
+using Dapper;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+
+namespace OrmApp;
+
+public class OrmBench
+{
+    private const string connectionString = "Server=localhost;Database=Owl;Trusted_Connection=True;TrustServerCertificate=True;";
+    
+    private static readonly Guid Id = Guid.Parse("01965E96-564D-7066-A9F7-88CAECF1E7CE");
+    
+    private AppDbContext dbContext;
+    private SqlConnection dbConnection;
+    
+    [GlobalSetup]
+    public void GlobalSetup()
+    {
+        dbContext = new AppDbContext(connectionString);
+        dbConnection = new SqlConnection(connectionString);
+        dbConnection.Open();
+    }
+    
+    [GlobalCleanup]
+    public void GlobalCleanup()
+    {
+        dbContext.Dispose();
+        dbConnection.Dispose();
+    }
+    
+    [Benchmark]
+    public async Task EF_SelectById()
+    {
+         _ = await dbContext.Users.AsNoTracking().FirstOrDefaultAsync(u => u.Id == Id);
+    }
+    
+    [Benchmark]
+    public async Task Dapper_SelectById()
+    {
+        _ = await dbConnection.QueryFirstOrDefaultAsync<User>(
+            "SELECT Id, Email FROM dbo.Users WHERE Id = @id",
+            new { Id });
+    }
+    
+    [Benchmark]
+    public async Task EF_SelectAll()
+    {
+        _ = await dbContext.Users.AsNoTracking().ToListAsync();
+    }
+    
+    [Benchmark]
+    public async Task Dapper_SelectAll()
+    {
+         _ = (await dbConnection.QueryAsync<User>("SELECT Id, Email FROM dbo.Users")).ToList();
+    }
+    
+    
+    [Benchmark]
+    public async Task EF_Insert()
+    {
+        dbContext.Users.Add(new User
+        {
+            Id = Guid.NewGuid(),
+            Email = $"{Guid.NewGuid()}@example.com"
+        });
+
+        await dbContext.SaveChangesAsync();
+    }
+    
+    [Benchmark]
+    public async Task Dapper_Insert()
+    {
+        await dbConnection.ExecuteAsync(
+            "INSERT INTO dbo.Users (Id, Email) VALUES (@id, @e);",
+            new { id = Guid.NewGuid(), e = $"{Guid.NewGuid()}@example.com" });
+    }
+}
