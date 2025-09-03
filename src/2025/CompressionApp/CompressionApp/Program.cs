693f4648530d0286812298cc26fc130bf0e63d81
Denis Makarenko <denis-makarenko@outlook.com>
2025-09-03 18:31:59 +0200

    Added compression

--- src/2025/CompressionApp/CompressionApp/Program.cs
+++ src/2025/CompressionApp/CompressionApp/Program.cs
@@ -1,6 +1,31 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+using System.IO.Compression;
+using CompressionApp;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.ResponseCompression;
 
-app.MapGet("/", () => "Hello World!");
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddScoped<UserFactory>();
+builder.Services.AddResponseCompression(options =>
+{
+    options.EnableForHttps = true;
+    
+    options.MimeTypes = ResponseCompressionDefaults.MimeTypes.Concat([
+        "application/json",
+        "image/svg+xml",
+        "application/javascript",
+        "text/css"
+    ]);
+});
+
+builder.Services.Configure<BrotliCompressionProviderOptions>(options => options.Level = CompressionLevel.Optimal);
+
+builder.Services.Configure<GzipCompressionProviderOptions>(options =>  options.Level = CompressionLevel.Optimal);
+
+var app = builder.Build();
+app.UseResponseCompression();
+
+app.MapGet("/users", ([FromServices] UserFactory userFactory)
+    => TypedResults.Ok(userFactory.Generate(100_000)));
 
 app.Run();
---
+++ src/2025/CompressionApp/CompressionApp/UserFactory.cs
@@ -0,0 +1,14 @@
+﻿using Bogus;
+
+namespace CompressionApp;
+
+public sealed class UserFactory : Faker<User>
+{
+    public UserFactory()
+    {
+        RuleFor(user => user.Id, _ => Guid.CreateVersion7());
+        RuleFor(user => user.FirstName, faker => faker.Name.FirstName());
+        RuleFor(user => user.LastName, faker => faker.Name.LastName());
+        RuleFor(user => user.Email, faker => faker.Internet.Email());
+    }
+}
