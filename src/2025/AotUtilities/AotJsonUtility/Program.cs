f8a447f404e724448428350da87266bd4c376e90
Denis Makarenko <denis-makarenko@outlook.com>
2025-09-07 22:54:07 +0200

    added utilites

--- src/2025/AotUtilities/AotJsonUtility/Program.cs
+++ src/2025/AotUtilities/AotJsonUtility/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Text.Json;
+
+var json = args[0];
+
+try
+{
+    var jsonDocument = JsonDocument.Parse(json);
+    var options = new JsonSerializerOptions { WriteIndented = true };
+    var pretty = JsonSerializer.Serialize(jsonDocument.RootElement, options);
+    Console.WriteLine(pretty);
+}
+catch (Exception exception)
+{
+    Console.Error.WriteLine($"Invalid JSON: {exception.Message}");
+}
