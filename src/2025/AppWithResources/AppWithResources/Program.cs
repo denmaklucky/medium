5c282bc150cafc48d3b7e03c8c7c38597af8443b
Denis Makarenko <denis-makarenko@outlook.com>
2025-10-03 15:02:27 +0200

    Added a new project - AppWithResources

---
+++ src/2025/AppWithResources/AppWithResources/Program.cs
@@ -0,0 +1,13 @@
+using AppWithResources;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Localization;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddLocalization(options => options.ResourcesPath = "Properties");
+
+var app = builder.Build();
+
+app.MapGet("/", ([FromServices] IStringLocalizer<ValidateError> localizer) => localizer["Error"].Value);
+
+app.Run();
---
+++ src/2025/AppWithResources/AppWithResources/ValidateError.cs
@@ -0,0 +1,3 @@
+﻿namespace AppWithResources;
+
+public sealed class ValidateError;
