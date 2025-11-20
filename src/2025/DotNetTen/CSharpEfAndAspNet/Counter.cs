5ca7f97edada9d053bcbb4c794b7ea941b1b7480
Denis Makarenko <denis-makarenko@outlook.com>
2025-11-20 18:57:34 +0100

    Some changes

---
+++ src/2025/DotNetTen/CSharpEfAndAspNet/Counter.cs
@@ -0,0 +1,24 @@
+﻿namespace CSharpEfAndAspNet;
+
+public sealed class Counter(int value)
+{
+    public int Value { get; } = value;
+    
+    public static Counter operator ++(Counter counter)
+    {
+        var currentValue = counter.Value;
+
+        currentValue += 1;
+
+        return new (currentValue);
+    }
+
+    public static Counter operator --(Counter counter)
+    {
+        var currentValue = counter.Value;
+
+        currentValue -= 1;
+
+        return new (currentValue);
+    }
+}
