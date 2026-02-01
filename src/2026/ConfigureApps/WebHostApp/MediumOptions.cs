9f234c9ab7af1ccd64323d9310dd74ce5fd21d48
Denis Makarenko <denis-makarenko@outlook.com>
2026-02-01 14:04:23 +0100

    Missing changes

--- src/2026/ConfigureApps/WebHostApp/MediumOptions.cs
+++ src/2026/ConfigureApps/WebHostApp/MediumOptions.cs
@@ -1,8 +1,11 @@
-﻿namespace WebHostApp;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebHostApp;
 
 internal sealed class MediumOptions
 {
     public static string SectionName = "Medium";
 
-    public Guid Key { get; set; }
+    [Required]
+    public Guid Key { get; init; }
 }
--- src/2026/ConfigureApps/WebHostApp/Program.cs
+++ src/2026/ConfigureApps/WebHostApp/Program.cs
@@ -12,11 +12,17 @@ configuration.GetSection(MediumOptions.SectionName).Bind(mediumOptions);
 
 var mediumOptions1 = configuration.GetSection(MediumOptions.SectionName).Get<MediumOptions>();
 
-builder.Services.Configure<MediumOptions>(
-    builder.Configuration.GetSection(MediumOptions.SectionName));
+builder.Services.Configure<MediumOptions>(builder.Configuration.GetSection(MediumOptions.SectionName));
+builder.Services
+    .AddOptions<MediumOptions>()
+    .ValidateDataAnnotations();
 
 var app = builder.Build();
 
 app.MapGet("/", ([FromServices] IOptions<MediumOptions> options) => options.Value.Key);
+
+app.MapGet("/v1", ([FromServices] IOptionsSnapshot<MediumOptions> options) => options.Value.Key);
+
+app.MapGet("/v2", ([FromServices] IOptionsMonitor<MediumOptions> options) => options.CurrentValue.Key);
 
 app.Run();
