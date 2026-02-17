e07b6348ba6e02aaae2346c243957669a813eb5b
Denis Makarenko <denis-makarenko@outlook.com>
2026-02-17 19:44:59 +0100

    added projects for new post - interactive multi-page app

---
+++ src/2026/InteractiveMpa/HydroApp/Pages/Privacy.cshtml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace HydroApp.Pages;
+
+public class PrivacyModel : PageModel
+{
+    public void OnGet()
+    {
+    }
+}
+
---
+++ src/2026/InteractiveMpa/RazorHtmxApp/Pages/Privacy.cshtml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RazorHtmxApp.Pages;
+
+public class PrivacyModel : PageModel
+{
+    public void OnGet()
+    {
+    }
+}
+
