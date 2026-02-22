785454ad78ce6d5234755c41a302f927e289a016
Denis Makarenko <denis-makarenko@outlook.com>
2026-02-22 11:56:22 +0100

    Added a ToDoList component in blazor app

--- src/2026/InteractiveMpa/BlazorApp/Program.cs
+++ src/2026/InteractiveMpa/BlazorApp/Program.cs
@@ -1,10 +1,13 @@
-using PureBlazorApp.Components;
+using BlazorApp.Components;
+using Shared;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+
+builder.Services.AddScoped<IToDoService, InMemoryToDoService>();
 
 var app = builder.Build();
 
